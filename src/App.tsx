a3a12582f8fced90bc259aa72476a347a4ea66de
andreycmartins <andreybr45@gmail.com>
2023-01-16 14:41:44 -0300

    Adicionado tema escuro e troca de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from "./routes"
+import { AppThemeProvider } from "./shared/contexts/ThemeContext"
+import { DarkTheme, LightTheme } from "./shared/themes"
+
 
 export const App = () => {
   return (
-    <>
+    <AppThemeProvider>
       <BrowserRouter>
-        <AppRoutes></AppRoutes>
+        <AppRoutes/>
       </BrowserRouter>
-    </>
+    </AppThemeProvider>
   )
 }
